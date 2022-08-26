c94763ec2deba5d21ad1ec0b955e726c02d8ff1d
Daisuke-Glitch <abdullahisalihuinusa@gmail.com>
2022-08-26 23:06:53 +0100

    first comment

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+console.log(
+  "%c Real website: https://zuri.team/",
+  "color:red; font-size: 1rem;"
+);
+
+console.log(
+  "%c Copyright Made By King Logic",
+  "color: deepskyblue; font-size: 1rem;"
+);
+
+console.log("How i made it");
+
+console.log(
+  "%c 1. I downloaded all the images.",
+  "color: orange;font-size: 0.7rem;"
+);
+
+console.log(
+  "%c 2. I used a chrome extension called color picker to get the exact color used.",
+  "color: orange;font-size: 0.7rem;"
+);
+
+console.log(
+  "%c 3. I also used the Poppins font cos I couldn't find the real font used.",
+  "color: orange;font-size: 0.7rem;"
+);
+
+console.log(
+  "%c 4. And lastly, I used bootstrap.",
+  "color: orange;font-size: 0.7rem;"
+);
